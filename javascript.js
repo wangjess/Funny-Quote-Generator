97cb744e81703cc2fc5a271d73dde1a202b936c4
Jessica <wangjess@umich.edu>
2018-08-25 23:18:42 -0400

    Added core files.
    Functional. But not very beautiful! Need to add some CSS :)

---
+++ javascript.js
@@ -0,0 +1,10 @@
+var quotes = [
+	'Do not take life too seriously. You will never get out of \it alive. - Elbert Hubbard',
+	'May the forces of evil become confused on the way to your house. - George Carlin',
+	'Laziness \is nothing more than the habit of resting before you get tired. - Jules Renard', 
+	'There can not be a crisis next week. My schedule \is already full. - Henry Kissinger',
+	'My life needs editing. - Mort Sahl',
+	'I always wanted to be somebody, but now \I realize \I should have been more specific. - Lily Tomin',
+	'If at first you don\'t succeed, find out \if the loser gets anything. - William Lyon Phelps',
+	'You\'re only as good as your last haircut. - Fran Lebowitz'
+]
